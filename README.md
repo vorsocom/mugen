3b7f153c4bfaf76971dfb3e4894587c73750cc9f
Sando George <sando.csc@gmail.com>
2024-08-19 01:59:32 -0400

    Bumped version to 0.12.0.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Gloria v0.11.0
+# Gloria v0.12.0
 
 Gloria is an AI assistant developed for the Matrix/Synapse/Element stack.
--- quartman.py
+++ quartman.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright © 2024, Vorso Computing, Inc."
 
 __email__ = "brightideas@vorsocomputing.com"
 
-__version__ = "0.11.0"
+__version__ = "0.12.0"
 
 import asyncio
 import os
