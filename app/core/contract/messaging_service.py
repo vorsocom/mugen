a49ada281cda0981edb83754e5b35f7e9aeda41b
Sando George <sando.csc@gmail.com>
2024-08-31 22:58:25 -0400

    feat: Added support for Context extensions.
    All the default system context is now provided by an extension.

--- app/core/contract/messaging_service.py
+++ app/core/contract/messaging_service.py
@@ -3,8 +3,10 @@
 __all__ = ["IMessagingService"]
 
 from abc import ABC, abstractmethod
-from app.core.contract.rag_extension import IRAGExtension
+
 from app.core.contract.ct_extension import ICTExtension
+from app.core.contract.ctx_extension import ICTXExtension
+from app.core.contract.rag_extension import IRAGExtension
 
 
 class IMessagingService(ABC):
@@ -21,8 +23,12 @@ class IMessagingService(ABC):
 
     @abstractmethod
     def register_ct_extension(self, ext: ICTExtension) -> None:
-        """Register a CT extension with the messaging service."""
+        """Register a Coversational Trigger (CT) extension."""
+
+    @abstractmethod
+    def register_ctx_extension(self, ext: ICTXExtension) -> None:
+        """Register a Context (CTX) extension."""
 
     @abstractmethod
     def register_rag_extension(self, ext: IRAGExtension) -> None:
-        """Register an RAG extension with the messaging service."""
+        """Register a Retrieval Augmented Generation (RAG) extension."""
