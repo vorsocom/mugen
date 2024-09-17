cf72b99126469a218a0cd1bfb06c9527a02c337f
Sando George <sando.csc@gmail.com>
2024-09-17 12:17:04 -0400

    feat: Implemented conditional loading of knowledge retrieval module and platform clients.

--- app/core/di/__init__.py
+++ app/core/di/__init__.py
@@ -22,6 +22,7 @@ from app.core.contract.user_service import IUserService
 from app.core.contract.whatsapp_client import IWhatsAppClient
 
 default_modules = SimpleNamespace(**json.loads(os.getenv("GLORIA_DEFAULT_MODULES")))
+platforms = json.loads(os.getenv("GLORIA_PLATFORMS"))
 
 import_module(name=default_modules.logging_gateway)
 logging_gateway_class = ILoggingGateway.__subclasses__()[0]
@@ -38,24 +39,43 @@ storage_gateway_class = IKeyValStorageGateway.__subclasses__()[0]
 import_module(name=default_modules.nlp_service)
 nlp_service_class = INLPService.__subclasses__()[0]
 
-import_module(name=default_modules.knowledge_retrieval)
-knowledge_retrieval_class = IKnowledgeRetrievalGateway.__subclasses__()[0]
-
 import_module(name=default_modules.user_service)
 user_service_class = IUserService.__subclasses__()[0]
 
 import_module(name=default_modules.messaging_service)
 messaging_service_class = IMessagingService.__subclasses__()[0]
 
-import_module(name=default_modules.matrix_client)
-matrix_client_class = AsyncClient.__subclasses__()[0]
+# pylint: disable=invalid-name
 
-import_module(name=default_modules.whatsapp_client)
-whatsapp_client_class = IWhatsAppClient.__subclasses__()[0]
+knowledge_retrieval_class = None
+if (
+    hasattr(default_modules, "knowledge_retrieval")
+    and default_modules.knowledge_retrieval != ""
+):
+    import_module(name=default_modules.knowledge_retrieval)
+    knowledge_retrieval_class = IKnowledgeRetrievalGateway.__subclasses__()[0]
 
-matrix_ipc_queue = asyncio.Queue()
+matrix_client_class = None
+matrix_ipc_queue = None
+if (
+    hasattr(default_modules, "matrix_client")
+    and default_modules.matrix_client != ""
+    and "matrix" in platforms
+):
+    import_module(name=default_modules.matrix_client)
+    matrix_client_class = AsyncClient.__subclasses__()[0]
+    matrix_ipc_queue = asyncio.Queue()
 
-whatsapp_ipc_queue = asyncio.Queue()
+whatsapp_client_class = None
+whatsapp_ipc_queue = None
+if (
+    hasattr(default_modules, "whatsapp_client")
+    and default_modules.whatsapp_client != ""
+    and "whatsapp" in platforms
+):
+    import_module(name=default_modules.whatsapp_client)
+    whatsapp_client_class = IWhatsAppClient.__subclasses__()[0]
+    whatsapp_ipc_queue = asyncio.Queue()
 
 
 # pylint: disable=c-extension-no-member
@@ -64,10 +84,6 @@ class DIContainer(containers.DeclarativeContainer):
     """An application-wide dependency injector container."""
 
     config = providers.Configuration()
-
-    matrix_ipc_queue = providers.Object(matrix_ipc_queue)
-
-    whatsapp_ipc_queue = providers.Object(whatsapp_ipc_queue)
 
     logging_gateway = providers.Singleton(
         logging_gateway_class,
@@ -97,13 +113,6 @@ class DIContainer(containers.DeclarativeContainer):
         logging_gateway=logging_gateway,
     )
 
-    knowledge_retrieval_gateway = providers.Singleton(
-        knowledge_retrieval_class,
-        config=config,
-        logging_gateway=logging_gateway,
-        nlp_service=nlp_service,
-    )
-
     user_service = providers.Singleton(
         user_service_class,
         keyval_storage_gateway=keyval_storage_gateway,
@@ -119,24 +128,44 @@ class DIContainer(containers.DeclarativeContainer):
         user_service=user_service,
     )
 
-    matrix_client = providers.Singleton(
-        matrix_client_class,
-        di_config=config,
-        ipc_queue=matrix_ipc_queue,
-        ipc_service=ipc_service,
-        keyval_storage_gateway=keyval_storage_gateway,
-        logging_gateway=logging_gateway,
-        messaging_service=messaging_service,
-        user_service=user_service,
-    )
+    if knowledge_retrieval_class:
+        knowledge_retrieval_gateway = providers.Singleton(
+            knowledge_retrieval_class,
+            config=config,
+            logging_gateway=logging_gateway,
+            nlp_service=nlp_service,
+        )
+    else:
+        knowledge_retrieval_gateway = providers.Object(None)
 
-    whatsapp_client = providers.Singleton(
-        whatsapp_client_class,
-        config=config,
-        ipc_queue=whatsapp_ipc_queue,
-        ipc_service=ipc_service,
-        keyval_storage_gateway=keyval_storage_gateway,
-        logging_gateway=logging_gateway,
-        messaging_service=messaging_service,
-        user_service=user_service,
-    )
+    if matrix_client_class:
+        matrix_ipc_queue = providers.Object(matrix_ipc_queue)
+        matrix_client = providers.Singleton(
+            matrix_client_class,
+            di_config=config,
+            ipc_queue=matrix_ipc_queue,
+            ipc_service=ipc_service,
+            keyval_storage_gateway=keyval_storage_gateway,
+            logging_gateway=logging_gateway,
+            messaging_service=messaging_service,
+            user_service=user_service,
+        )
+    else:
+        matrix_client = providers.Object(None)
+        matrix_ipc_queue = providers.Object(None)
+
+    if whatsapp_client_class:
+        whatsapp_ipc_queue = providers.Object(whatsapp_ipc_queue)
+        whatsapp_client = providers.Singleton(
+            whatsapp_client_class,
+            config=config,
+            ipc_queue=whatsapp_ipc_queue,
+            ipc_service=ipc_service,
+            keyval_storage_gateway=keyval_storage_gateway,
+            logging_gateway=logging_gateway,
+            messaging_service=messaging_service,
+            user_service=user_service,
+        )
+    else:
+        whatsapp_client = providers.Object(None)
+        whatsapp_ipc_queue = providers.Object(None)
