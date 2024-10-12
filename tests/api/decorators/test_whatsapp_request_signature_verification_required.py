1d83ac771c519e0f422819f99aa11cf7df859421
Sando George <sando.csc@gmail.com>
2024-10-12 06:19:41 -0400

    test: Signature verification 401 response.

--- tests/api/decorators/test_whatsapp_request_signature_verification_required.py
+++ tests/api/decorators/test_whatsapp_request_signature_verification_required.py
@@ -59,8 +59,50 @@ class TestWhatsAppRequestSigVerificationRequired(unittest.IsolatedAsyncioTestCas
             with self.assertRaises(werkzeug.exceptions.BadRequest):
                 await endpoint()
 
-    async def test_x_hub_signature_header_is_set(self):
-        """Test output when X-Hub-Signature header is set."""
+    async def test_x_hub_signature_header_is_set_incorrect_hash(self):
+        """Test output when X-Hub-Signature header is set with incorrect hash."""
+        # Create dummy app to get context.
+        app = Quart("test")
+
+        # Generate token to use as app secret.
+        app_secret = secrets.token_urlsafe()
+
+        # Create dummy config object to patch current_app.config.
+        app.config = app.config | {
+            "ENV": SimpleNamespace(
+                whatsapp=SimpleNamespace(
+                    app=SimpleNamespace(
+                        secret=lambda: app_secret,
+                    ),
+                ),
+            ),
+        }
+
+        # Set data and calculate hex digest to create dummy request.
+        request_data = "test data"
+
+        # Create header object for request context.
+        headers = {
+            "X-Hub-Signature-256": "incorrecthashlksdk030920",
+        }
+
+        # Use dummy app context.
+        async with app.app_context(), app.test_request_context(
+            "test",
+            data=request_data,
+            headers=headers,
+        ):
+            # Define and patch dummy endpoint.
+            @unittest.mock.patch(target="quart.current_app.logger")
+            @whatsapp_request_signature_verification_required
+            async def endpoint(*args, **kwargs):
+                pass
+
+            with self.assertRaises(werkzeug.exceptions.Unauthorized):
+                await endpoint()
+
+    async def test_x_hub_signature_header_is_set_correct_hash(self):
+        """Test output when X-Hub-Signature header is set with correct hash."""
         # Create dummy app to get context.
         app = Quart("test")
 
