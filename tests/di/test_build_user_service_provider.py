4b91be9ccd544dcf628fc7f4e7353f47501bb62f
Sando George <sando.csc@gmail.com>
2024-10-17 14:38:27 -0400

    test: Added tests for mugen.core.di._build_user_service_provider.

---
+++ tests/di/test_build_user_service_provider.py
@@ -0,0 +1,248 @@
+"""Provides unit tests for mugen.core.di._build_user_service."""
+
+import unittest
+import unittest.mock
+
+from mugen.core import di
+from mugen.core.contract.service.user import IUserService
+
+
+# pylint: disable=protected-access
+class TestDIBuildUserService(unittest.TestCase):
+    """Unit tests for mugen.core.di._build_user_service."""
+
+    def test_module_configuration_unavailable(self):
+        """Test effects of missing module configuration."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Empty config.
+                config = {}
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Attempt to build the user service.
+                di._build_user_service_provider(config, injector)
+
+                # The root logger should be used since the name
+                # of the muGen logger is not available from the
+                # config.
+                self.assertEqual(logger.records[0].name, "root")
+
+                # The user service cannot be configured since
+                # there is no configuration specifying the module.
+                self.assertEqual(
+                    logger.output[0],
+                    "ERROR:root:Invalid configuration (user_service).",
+                )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_module_import_failure(self):
+        """Test effects of module import failure."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "service": {
+                                    "user": "nonexistent_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Attempt to build the user service.
+                di._build_user_service_provider(config, injector)
+
+                # The root logger should be used since the name
+                # of the muGen logger is not available from the
+                # config.
+                self.assertEqual(logger.records[0].name, "root")
+
+                # The user service module cannot be imported
+                # since a nonexistent module was supplied.
+                self.assertEqual(
+                    logger.output[0],
+                    "ERROR:root:Could not import module (user_service).",
+                )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_incorrectly_typed_injector(self):
+        """Test effects of an incorrectly typed injector."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "service": {
+                                    "user": "valid_user_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = None
+
+                with unittest.mock.patch.dict(
+                    "sys.modules",
+                    {
+                        "valid_user_module": unittest.mock.Mock(),
+                    },
+                ):
+                    # Attempt to build the user service.
+                    di._build_user_service_provider(config, injector)
+
+                    # The root logger should be used since the name
+                    # of the muGen logger is not available from the
+                    # config.
+                    self.assertEqual(logger.records[0].name, "root")
+
+                    # The operation cannot be completed since the
+                    # injector is incorrectly typed and "config=injector.config"
+                    # will fail.
+                    self.assertEqual(
+                        logger.output[0],
+                        "ERROR:root:Invalid injector (user_service).",
+                    )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_valid_subclass_not_found(self):
+        """Test effects of invalid subclass import."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "service": {
+                                    "user": "valid_user_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Dummy subclasses
+                sc = unittest.mock.Mock
+                sc.return_value = []
+
+                with (
+                    unittest.mock.patch.dict(
+                        "sys.modules",
+                        {
+                            "valid_user_module": unittest.mock.Mock(),
+                        },
+                    ),
+                    unittest.mock.patch(
+                        target=(  # pylint: disable=line-too-long
+                            "mugen.core.contract.service.user.IUserService.__subclasses__"
+                        ),
+                        new_callable=sc,
+                    ),
+                ):
+                    # Attempt to build the user service.
+                    di._build_user_service_provider(config, injector)
+
+                    # The root logger should be used since the name
+                    # of the muGen logger is not available from the
+                    # config.
+                    self.assertEqual(logger.records[0].name, "root")
+
+                    # The operation cannot be completed since a valid
+                    # subclass would not be found.
+                    self.assertEqual(
+                        logger.output[0],
+                        "ERROR:root:Valid subclass not found (user_service).",
+                    )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_valid_subclass_available(self):
+        """Test effects of a valid subclass being available."""
+        try:
+            # No logs expected.
+            with self.assertNoLogs("root", level="ERROR"):
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "service": {
+                                    "user": "valid_user_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Dummy subclasses
+                # pylint: disable=too-few-public-methods
+                class DummyUserClass(IUserService):
+                    """Dummy user class."""
+
+                    def __init__(self, keyval_storage_gateway, logging_gateway):
+                        pass
+
+                    def add_known_user(self, user_id, displayname, room_id):
+                        pass
+
+                    def get_known_users_list(self):
+                        pass
+
+                    def get_user_display_name(self, user_id):
+                        pass
+
+                    def save_known_users_list(self, known_users):
+                        pass
+
+                sc = unittest.mock.Mock
+                sc.return_value = [DummyUserClass]
+
+                with (
+                    unittest.mock.patch.dict(
+                        "sys.modules",
+                        {
+                            "valid_user_module": unittest.mock.Mock(),
+                        },
+                    ),
+                    unittest.mock.patch(
+                        target=(  # pylint: disable=line-too-long
+                            "mugen.core.contract.service.user.IUserService.__subclasses__"
+                        ),
+                        new_callable=sc,
+                    ),
+                ):
+                    # Attempt to build the user service.
+                    di._build_user_service_provider(config, injector)
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
