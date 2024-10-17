a79b1249625d403162194342827cde60ee15bc98
Sando George <sando.csc@gmail.com>
2024-10-17 11:07:50 -0400

    test: Added tests for mugen.core.di._build_completion_gateway_provider.

--- mugen/core/di/__init__.py
+++ mugen/core/di/__init__.py
@@ -128,13 +128,47 @@ def _build_completion_gateway_provider(
     config: dict,
     injector: DependencyInjector,
 ) -> None:
-    """"""
-    import_module(name=config["mugen"]["modules"]["core"]["gateway"]["completion"])
+    """Build logging gateway provider for DI container."""
+    # Get logger.
+    # Exceptions/conditions are for a
+    # testing environment.
+    try:
+        logger = injector.logging_gateway
+        if not logger:
+            logger = logging.getLogger()
+    except AttributeError:
+        logger = logging.getLogger()
 
-    injector.completion_gateway = ICompletionGateway.__subclasses__()[0](
-        config=injector.config,
-        logging_gateway=injector.logging_gateway,
-    )
+    try:
+        try:
+            import_module(
+                name=config["mugen"]["modules"]["core"]["gateway"]["completion"]
+            )
+        except KeyError:
+            logger.error("Invalid configuration (completion_gateway).")
+            return
+    except ModuleNotFoundError:
+        # This could fail due to missing configuration values or
+        # invalid module paths. Either way, no need to continue
+        # if it fails.
+        logger.error("Could not import module (completion_gateway).")
+        return
+
+    try:
+        try:
+            injector.completion_gateway = ICompletionGateway.__subclasses__()[0](
+                config=injector.config,
+                logging_gateway=injector.logging_gateway,
+            )
+        except AttributeError:
+            # We'll get an AttributeError if injector
+            # is incorrectly typed.
+            logger.error("Invalid injector (completion_gateway).")
+            return
+    except IndexError:
+        # We'll get an IndexError if the imported module
+        # doesn't provide a subclass of ILoggingGateway.
+        logger.error("Valid subclass not found (completion_gateway).")
 
 
 def _build_ipc_service_provider(
---
+++ tests/di/test_build_completion_gateway.py
@@ -0,0 +1,235 @@
+"""Provides unit tests for mugen.core.di._build_completion_gateway."""
+
+import unittest
+import unittest.mock
+
+from mugen.core import di
+from mugen.core.contract.gateway.completion import ICompletionGateway
+
+
+# pylint: disable=protected-access
+class TestDIBuildCompletionGateway(unittest.TestCase):
+    """Unit tests for mugen.core.di._build_completion_gateway."""
+
+    def test_module_configuration_unavailable(self):
+        """Test effects of missing module configuration."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Empty config.
+                config = {}
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Attempt to build the completion gateway.
+                di._build_completion_gateway_provider(config, injector)
+
+                # The root logger should be used since the name
+                # of the muGen logger is not available from the
+                # config.
+                self.assertEqual(logger.records[0].name, "root")
+
+                # The completion gateway cannot be configured since
+                # there is no configuration specifying the module.
+                self.assertEqual(
+                    logger.output[0],
+                    "ERROR:root:Invalid configuration (completion_gateway).",
+                )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_module_import_failure(self):
+        """Test effects of module import failure."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "gateway": {
+                                    "completion": "nonexistent_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Attempt to build the completion gateway.
+                di._build_completion_gateway_provider(config, injector)
+
+                # The root logger should be used since the name
+                # of the muGen logger is not available from the
+                # config.
+                self.assertEqual(logger.records[0].name, "root")
+
+                # The completion gateway module cannot be imported
+                # since a nonexistent module was supplied.
+                self.assertEqual(
+                    logger.output[0],
+                    "ERROR:root:Could not import module (completion_gateway).",
+                )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_incorrectly_typed_injector(self):
+        """Test effects of an incorrectly typed injector."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "gateway": {
+                                    "completion": "valid_completion_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = None
+
+                with unittest.mock.patch.dict(
+                    "sys.modules",
+                    {
+                        "valid_completion_module": unittest.mock.Mock(),
+                    },
+                ):
+                    # Attempt to build the completion gateway.
+                    di._build_completion_gateway_provider(config, injector)
+
+                    # The root logger should be used since the name
+                    # of the muGen logger is not available from the
+                    # config.
+                    self.assertEqual(logger.records[0].name, "root")
+
+                    # The operation cannot be completed since the
+                    # injector is incorrectly typed and "config=injector.config"
+                    # will fail.
+                    self.assertEqual(
+                        logger.output[0],
+                        "ERROR:root:Invalid injector (completion_gateway).",
+                    )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_valid_subclass_not_found(self):
+        """Test effects of invalid subclass import."""
+        try:
+            with self.assertLogs("root", level="ERROR") as logger:
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "gateway": {
+                                    "completion": "valid_completion_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Dummy subclasses
+                sc = unittest.mock.Mock
+                sc.return_value = []
+
+                with (
+                    unittest.mock.patch.dict(
+                        "sys.modules",
+                        {
+                            "valid_completion_module": unittest.mock.Mock(),
+                        },
+                    ),
+                    unittest.mock.patch(
+                        target="mugen.core.contract.gateway.completion.ICompletionGateway.__subclasses__",  # pylint: disable=line-too-long
+                        new_callable=sc,
+                    ),
+                ):
+                    # Attempt to build the completion gateway.
+                    di._build_completion_gateway_provider(config, injector)
+
+                    # The root logger should be used since the name
+                    # of the muGen logger is not available from the
+                    # config.
+                    self.assertEqual(logger.records[0].name, "root")
+
+                    # The operation cannot be completed since a valid
+                    # subclass of ILoggingGateway would not be found.
+                    self.assertEqual(
+                        logger.output[0],
+                        "ERROR:root:Valid subclass not found (completion_gateway).",
+                    )
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
+
+    def test_valid_subclass_available(self):
+        """Test effects of a valid subclass being available."""
+        try:
+            # No logs expected.
+            with self.assertNoLogs("root", level="ERROR"):
+                # Logging config.
+                config = {
+                    "mugen": {
+                        "modules": {
+                            "core": {
+                                "gateway": {
+                                    "completion": "valid_completion_module",
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # New injector
+                injector = di.injector.DependencyInjector()
+
+                # Dummy subclasses
+                # pylint: disable=too-few-public-methods
+                class DummyCompletionClass(ICompletionGateway):
+                    """Dummy completion class."""
+
+                    def __init__(self, config, logging_gateway):
+                        pass
+
+                    async def get_completion(self, context, operation="completion"):
+                        pass
+
+                sc = unittest.mock.Mock
+                sc.return_value = [DummyCompletionClass]
+
+                with (
+                    unittest.mock.patch.dict(
+                        "sys.modules",
+                        {
+                            "valid_completion_module": unittest.mock.Mock(),
+                        },
+                    ),
+                    unittest.mock.patch(
+                        target="mugen.core.contract.gateway.completion.ICompletionGateway.__subclasses__",  # pylint: disable=line-too-long
+                        new_callable=sc,
+                    ),
+                ):
+                    # Attempt to build the completion gateway.
+                    di._build_completion_gateway_provider(config, injector)
+        except:  # pylint: disable=bare-except
+            # We should not get here because all exceptions
+            # should be handled in the called function.
+            self.fail("Exception raised unexpectedly.")
