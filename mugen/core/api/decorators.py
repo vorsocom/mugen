d180e5832c8b15d7ec6b0a9bb20cd4144d60f4fd
Sando George <sando.csc@gmail.com>
2024-10-22 15:38:24 -0400

    test: Updated API decorator tests.

--- mugen/core/api/decorators.py
+++ mugen/core/api/decorators.py
@@ -5,6 +5,7 @@ import hashlib
 import hmac
 import ipaddress
 import os
+from types import SimpleNamespace
 
 
 from quart import abort, current_app, request
@@ -12,14 +13,17 @@ from quart import abort, current_app, request
 from mugen.core import di
 
 
-def matrix_platform_required(arg=None):
+def matrix_platform_required(
+    arg=None,
+    config: SimpleNamespace = di.container.config,
+):
     """Check that the Matrix platform is enabled."""
 
     def decorator(func):
         @wraps(func)
-        async def decorated(*args, **kwargs):
+        async def wrapper(*args, **kwargs):
             try:
-                if "matrix" not in di.container.config.mugen.platforms:
+                if "matrix" not in config.mugen.platforms:
                     current_app.logger.error("Matrix platform not enabled.")
                     abort(501)
                 return await func(*args, **kwargs)
@@ -27,19 +31,25 @@ def matrix_platform_required(arg=None):
                 current_app.logger.error("Could not get platform configuration.")
                 abort(500)
 
-        return decorated
+        return wrapper
 
-    return decorator(arg)
+    if callable(arg):
+        return decorator(arg)
+
+    return decorator
 
 
-def telnet_platform_required(arg=None):
+def telnet_platform_required(
+    arg=None,
+    config: SimpleNamespace = di.container.config,
+):
     """Check that the Telnet platform is enabled."""
 
     def decorator(func):
         @wraps(func)
-        async def decorated(*args, **kwargs):
+        async def wrapper(*args, **kwargs):
             try:
-                if "telnet" not in di.container.config.mugen.platforms:
+                if "telnet" not in config.mugen.platforms:
                     current_app.logger.error("Telnet platform not enabled.")
                     abort(501)
                 return await func(*args, **kwargs)
@@ -47,19 +57,25 @@ def telnet_platform_required(arg=None):
                 current_app.logger.error("Could not get platform configuration.")
                 abort(500)
 
-        return decorated
+        return wrapper
 
-    return decorator(arg)
+    if callable(arg):
+        return decorator(arg)
+
+    return decorator
 
 
-def whatsapp_platform_required(arg=None):
+def whatsapp_platform_required(
+    arg=None,
+    config: SimpleNamespace = di.container.config,
+):
     """Check that the WhatsApp platform is enabled."""
 
     def decorator(func):
         @wraps(func)
-        async def decorated(*args, **kwargs):
+        async def wrapper(*args, **kwargs):
             try:
-                if "whatsapp" not in di.container.config.mugen.platforms:
+                if "whatsapp" not in config.mugen.platforms:
                     current_app.logger.error("WhatsApp platform not enabled.")
                     abort(501)
                 return await func(*args, **kwargs)
@@ -67,67 +83,25 @@ def whatsapp_platform_required(arg=None):
                 current_app.logger.error("Could not get platform configuration.")
                 abort(500)
 
-        return decorated
+        return wrapper
 
-    return decorator(arg)
+    if callable(arg):
+        return decorator(arg)
+
+    return decorator
 
 
-def whatsapp_server_ip_allow_list_required(arg=None):
+def whatsapp_request_signature_verification_required(
+    arg=None,
+    config: SimpleNamespace = di.container.config,
+):
     """Authenticate requests to the webhook using app secret."""
 
     def decorator(func):
         @wraps(func)
-        async def decorated(*args, **kwargs):
+        async def wrapper(*args, **kwargs):
             try:
-                allow_list_path = di.container.config.whatsapp.servers.allowed
-                basedir = current_app.config["BASEDIR"]
-                networks: list
-                with open(
-                    f"{basedir}{os.sep}{allow_list_path}", "r", encoding="utf8"
-                ) as f:
-                    networks = [l.rstrip() for l in f]
-            except (AttributeError, FileNotFoundError, IsADirectoryError, KeyError):
-                current_app.logger.error("WhatsApp servers allow list not found.")
-                abort(500)
-
-            verification_required: str
-            try:
-                verification_required = current_app.config[
-                    "ENV"
-                ].whatsapp.servers.verify_ip()
-            except (AttributeError, KeyError):
-                current_app.logger.error(
-                    "WhatsApp ip verification requirement unknown."
-                )
-                verification_required = None
-
-            if verification_required is True:
-                remote_addr = request.headers["Remote-Addr"]
-                hits = [
-                    x
-                    for x in networks
-                    if ipaddress.ip_address(remote_addr) in ipaddress.ip_network(x)
-                ]
-
-                if len(hits) == 0:
-                    current_app.logger.error("Remote address not in allow list.")
-                    abort(500)
-
-            return await func(*args, **kwargs)
-
-        return decorated
-
-    return decorator(arg)
-
-
-def whatsapp_request_signature_verification_required(arg=None):
-    """Authenticate requests to the webhook using app secret."""
-
-    def decorator(func):
-        @wraps(func)
-        async def decorated(*args, **kwargs):
-            try:
-                app_secret = di.container.config.whatsapp.app.secret
+                app_secret = config.whatsapp.app.secret
             except (AttributeError, KeyError):
                 current_app.logger.error("WhatsApp app secret not found.")
                 abort(500)
@@ -152,6 +126,61 @@ def whatsapp_request_signature_verification_required(arg=None):
 
             return await func(*args, **kwargs)
 
-        return decorated
+        return wrapper
 
-    return decorator(arg)
+    if callable(arg):
+        return decorator(arg)
+
+    return decorator
+
+
+def whatsapp_server_ip_allow_list_required(
+    arg=None,
+    config: SimpleNamespace = di.container.config,
+):
+    """Authenticate requests to the webhook using app secret."""
+
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            try:
+                allow_list_path = config.whatsapp.servers.allowed
+                basedir = config.basedir
+                networks: list
+                with open(
+                    f"{basedir}{os.sep}{allow_list_path}", "r", encoding="utf8"
+                ) as f:
+                    networks = [l.rstrip() for l in f]
+            except (AttributeError, FileNotFoundError, IsADirectoryError, KeyError):
+                current_app.logger.error("WhatsApp servers allow list not found.")
+                abort(500)
+
+            verification_required: str
+            try:
+                verification_required = config.whatsapp.servers.verify_ip
+            except (AttributeError, KeyError):
+                current_app.logger.error(
+                    "WhatsApp ip verification requirement unknown."
+                )
+                verification_required = None
+
+            if verification_required is True:
+                remote_addr = request.headers["Remote-Addr"]
+                hits = [
+                    x
+                    for x in networks
+                    if ipaddress.ip_address(remote_addr) in ipaddress.ip_network(x)
+                ]
+
+                if len(hits) == 0:
+                    current_app.logger.error("Remote address not in allow list.")
+                    abort(500)
+
+            return await func(*args, **kwargs)
+
+        return wrapper
+
+    if callable(arg):
+        return decorator(arg)
+
+    return decorator
--- tests/api/decorators/test_whatsapp_server_ip_allow_list_required.py
+++ tests/api/decorators/test_whatsapp_server_ip_allow_list_required.py
@@ -1,8 +1,7 @@
 """Provides unit tests for whatsapp_server_ip_allow_list_required decorator."""
 
-import unittest
 from types import SimpleNamespace
-import unittest.mock
+import unittest
 import unittest.mock
 
 from quart import Quart
@@ -18,95 +17,91 @@ class TestWhatsAppServerIPAllowListRequired(unittest.IsolatedAsyncioTestCase):
     async def test_config_variable_not_set(self):
         """Test decorator output when whatsapp.servers.allowed is not set."""
         # Create dummy app to get context.
-        app = Quart("test")
+        app = Quart("test_app")
+
+        # Create dummy config for testing.
+        config = SimpleNamespace()
 
         async with app.app_context():
 
             # Define and patch dummy endpoint.
-            @unittest.mock.patch(target="quart.current_app.logger")
-            @whatsapp_server_ip_allow_list_required
-            async def endpoint(*args, **kwargs):
+            @whatsapp_server_ip_allow_list_required(config=config)
+            async def endpoint(*_args, **_kwargs):
                 pass
 
-            with self.assertRaises(werkzeug.exceptions.InternalServerError):
+            with (
+                self.assertLogs(logger="test_app", level="ERROR"),
+                self.assertRaises(werkzeug.exceptions.InternalServerError),
+            ):
                 await endpoint()
 
     async def test_allow_list_file_not_found(self):
         """Test decorator output when whatsapp.servers.allowed file is not found."""
         # Create dummy app to get context.
-        app = Quart("test")
+        app = Quart("test_app")
 
-        # Create dummy config object to patch current_app.config.
-        app.config = app.config | {
-            "BASEDIR": "",
-            "ENV": SimpleNamespace(
-                whatsapp=SimpleNamespace(
-                    servers=SimpleNamespace(
-                        allowed=lambda: "data/test_file.txt",
-                    ),
+        # Create dummy config for testing.
+        config = SimpleNamespace(
+            whatsapp=SimpleNamespace(
+                servers=SimpleNamespace(
+                    allowed="data/test_file.txt",
                 ),
             ),
-        }
+        )
 
         async with app.app_context():
 
             # Define and patch dummy endpoint.
-            @unittest.mock.patch(target="quart.current_app.logger")
-            @whatsapp_server_ip_allow_list_required
-            async def endpoint(*args, **kwargs):
+            @whatsapp_server_ip_allow_list_required(config=config)
+            async def endpoint(*_args, **_kwargs):
                 pass
 
-            with self.assertRaises(werkzeug.exceptions.InternalServerError):
+            with (
+                self.assertLogs(logger="test_app", level="ERROR"),
+                self.assertRaises(werkzeug.exceptions.InternalServerError),
+            ):
                 await endpoint()
 
     async def test_verification_required_flag_not_set(self):
         """Test decorator output when verification is not required."""
         # Create dummy app to get context.
-        app = Quart("test")
+        app = Quart("test_app")
 
-        # Create dummy config object to patch current_app.config.
-        app.config = app.config | {
-            "BASEDIR": "",
-            "ENV": SimpleNamespace(
-                whatsapp=SimpleNamespace(
-                    servers=SimpleNamespace(
-                        allowed=lambda: "",
-                    ),
+        # Create dummy config for testing.
+        config = SimpleNamespace(
+            basedir="",
+            whatsapp=SimpleNamespace(
+                servers=SimpleNamespace(
+                    allowed="",
                 ),
             ),
-        }
+        )
 
         async with app.app_context():
 
             # Define and patch dummy endpoint.
             @unittest.mock.patch(target="builtins.open")
-            @unittest.mock.patch(target="quart.current_app.logger")
-            @whatsapp_server_ip_allow_list_required
-            async def endpoint(*args, **kwargs):
+            @whatsapp_server_ip_allow_list_required(config=config)
+            async def endpoint(*_args, **_kwargs):
                 pass
 
-            try:
+            with self.assertLogs(logger="test_app", level="ERROR"):
                 await endpoint()
-            except:
-                self.fail("Exception raised unexpectedly.")
 
     async def test_verification_required_flag_is_set_invalid_ip(self):
         """Test decorator output when verification is required and ip is invalid."""
         # Create dummy app to get context.
-        app = Quart("test")
+        app = Quart("test_app")
 
-        # Create dummy config object to patch current_app.config.
-        app.config = app.config | {
-            "BASEDIR": "",
-            "ENV": SimpleNamespace(
-                whatsapp=SimpleNamespace(
-                    servers=SimpleNamespace(
-                        allowed=lambda: "",
-                        verify_ip=lambda: True,
-                    ),
+        # Create dummy config for testing.
+        config = SimpleNamespace(
+            whatsapp=SimpleNamespace(
+                servers=SimpleNamespace(
+                    allowed="",
+                    verify_ip=True,
                 ),
             ),
-        }
+        )
 
         # Dummy file
         dummy_file = unittest.mock.mock_open(read_data="192.168.0.0/24")
@@ -123,31 +118,31 @@ class TestWhatsAppServerIPAllowListRequired(unittest.IsolatedAsyncioTestCase):
         ):
             # Define and patch dummy endpoint.
             @unittest.mock.patch(target="builtins.open", new=dummy_file)
-            @unittest.mock.patch(target="quart.current_app.logger")
-            @whatsapp_server_ip_allow_list_required
-            async def endpoint(*args, **kwargs):
+            @whatsapp_server_ip_allow_list_required(config=config)
+            async def endpoint(*_args, **_kwargs):
                 pass
 
-            with self.assertRaises(werkzeug.exceptions.InternalServerError):
+            with (
+                self.assertLogs(logger="test_app", level="ERROR"),
+                self.assertRaises(werkzeug.exceptions.InternalServerError),
+            ):
                 await endpoint()
 
     async def test_verification_required_flag_is_set_valid_ip(self):
         """Test decorator output when verification is required and ip is valid."""
         # Create dummy app to get context.
-        app = Quart("test")
+        app = Quart("test_app")
 
-        # Create dummy config object to patch current_app.config.
-        app.config = app.config | {
-            "BASEDIR": "",
-            "ENV": SimpleNamespace(
-                whatsapp=SimpleNamespace(
-                    servers=SimpleNamespace(
-                        allowed=lambda: "",
-                        verify_ip=lambda: True,
-                    ),
+        # Create dummy config for testing.
+        config = SimpleNamespace(
+            basedir="",
+            whatsapp=SimpleNamespace(
+                servers=SimpleNamespace(
+                    allowed="",
+                    verify_ip=True,
                 ),
             ),
-        }
+        )
 
         # Dummy file
         dummy_file = unittest.mock.mock_open(read_data="127.0.0.0/8")
@@ -164,12 +159,9 @@ class TestWhatsAppServerIPAllowListRequired(unittest.IsolatedAsyncioTestCase):
         ):
             # Define and patch dummy endpoint.
             @unittest.mock.patch(target="builtins.open", new=dummy_file)
-            @unittest.mock.patch(target="quart.current_app.logger")
-            @whatsapp_server_ip_allow_list_required
-            async def endpoint(*args, **kwargs):
+            @whatsapp_server_ip_allow_list_required(config=config)
+            async def endpoint(*_args, **_kwargs):
                 pass
 
-            try:
+            with self.assertNoLogs():
                 await endpoint()
-            except:
-                self.fail("Exception raised unexpectedly.")
