6a2312a62b1ee2d80421b0777e96ea5f99aa3ff7
Sando George <sando.csc@gmail.com>
2026-06-06 12:13:27 -0400

    chore(release): prepare 0.51.0

--- quartman.py
+++ quartman.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright © 2025, Vorso Computing, Inc."
 
 __email__ = "brightideas@vorsocomputing.com"
 
-__version__ = "0.50.0"
+__version__ = "0.51.0"
 
 import asyncio
 import logging
